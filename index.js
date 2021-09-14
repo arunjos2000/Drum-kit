fa6593964169edb48427bd8300e9402a462ff897
arunjos2000 <arunjoseph.14.aj@gmail.com>
2021-09-14 21:06:01 +0530

    Tweeks made

--- index.js
+++ index.js
@@ -3,12 +3,14 @@ for(var i=0;i<numberOfDrums;i++){
   document.querySelectorAll(".drum")[i].addEventListener("click", function(){
         var buttonInnerHTML= this.innerHTML;
 makeSound(buttonInnerHTML);
+buttonAnimation(buttonInnerHTML);
 
   });
 }
 
 document.addEventListener("keydown",function(event){
         makeSound(event.key);
+        buttonAnimation(event.key);
 });
 function makeSound(key){
         switch(key){
@@ -43,4 +45,12 @@ function makeSound(key){
                 default:  console.log(innerHTML);
                               break;
                }     
+}
+function buttonAnimation(currentKey){
+
+        var activeButton =document.querySelector("."+ currentKey);
+        activeButton.classList.add("pressed");
+        setTimeout(() => {
+                activeButton.classList.remove("pressed");  
+        }, 100);
 }
